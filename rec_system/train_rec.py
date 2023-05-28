942021f3749bdeb2b6972ca962a3a0f520289003
Lazarev Volodymyr <vovalaz321@gmail.com>
2023-05-28 17:14:11 +0300

    working model and fixed requirements

--- rec_system/train_rec.py
+++ rec_system/train_rec.py
@@ -1,76 +1,89 @@
+import tez
+import pandas as pd
+from sklearn import model_selection
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import pandas as pd
+from sklearn import metrics, preprocessing
+import numpy as np
 
 
-class RecommendationModel(nn.Module):
-    def __init__(self, num_users, num_items, embedding_dim):
-        super(RecommendationModel, self).__init__()
+class MovieDataset:
+    def __init__(self, users, movies, ratings) -> None:
+        self.users = users
+        self.movies = movies
+        self.ratings = ratings
 
-        self.user_embedding = nn.Embedding(num_users, embedding_dim)
-        self.item_embedding = nn.Embedding(num_items, embedding_dim)
-        self.user_content_embedding = nn.Embedding(num_users, embedding_dim)
-        self.item_content_embedding = nn.Embedding(num_items, embedding_dim)
-        self.fc1 = nn.Linear(embedding_dim, 64)
-        self.fc2 = nn.Linear(64, embedding_dim)
+    def __len__(self):
+        return len(self.users)
 
-    def forward(self, user_indices, item_indices):
-        user_embed = self.user_embedding(user_indices)
-        item_embed = self.item_embedding(item_indices)
-        cf_output = torch.sum(user_embed * item_embed, dim=1)
+    def __getitem__(self, item):
+        user = self.users[item]
+        movie = self.movies[item]
+        rating = self.ratings[item]
 
-        user_content_embed = self.user_content_embedding(user_indices)
-        item_content_embed = self.item_content_embedding(item_indices)
-        cb_input = torch.cat((user_content_embed, item_content_embed), dim=1)
-        cb_output = F.relu(self.fc1(cb_input))
-        cb_output = self.fc2(cb_output)
-
-        output = cf_output + cb_output
-
-        return output
+        return {
+            "users": torch.tensor(user, dtype=torch.long),
+            "movies": torch.tensor(movie, dtype=torch.long),
+            "ratings": torch.tensor(rating, dtype=torch.float),
+        }
 
 
-# Load movie details from CSV
-movies_df = pd.read_csv("movies.csv")
-num_movies = len(movies_df)
+class RecSysModel(tez.Model):
+    def __init__(self, num_users, num_movies):
+        super().__init__()
+        self.user_embed = nn.Embedding(num_users, 32)
+        self.movie_embed = nn.Embedding(num_movies, 32)
+        self.out = nn.Linear(64, 1)
+        self.step_scheduler_after = "epoch"
 
-# Load user movie ratings from CSV
-ratings_df = pd.read_csv("ratings.csv")
-num_users = ratings_df["user_id"].nunique()
+    def fetch_optimizer(self):
+        opt = torch.optim.Adam(self.parameters(), lr=1e-4)
+        return opt
 
-# Map movie and user IDs to sequential indices
-movies_df["movie_index"] = pd.factorize(movies_df["movie_id"])[0]
-ratings_df["user_index"] = pd.factorize(ratings_df["user_id"])[0]
-ratings_df["movie_index"] = pd.factorize(ratings_df["movie_id"])[0]
+    def fetch_scheduler(self):
+        sch = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=3, gamma=0.7)
+        return sch
 
-# Prepare the data for training
-user_indices = torch.tensor(ratings_df["user_index"].values, dtype=torch.long)
-item_indices = torch.tensor(ratings_df["movie_index"].values, dtype=torch.long)
-ratings = torch.tensor(ratings_df["rating"].values, dtype=torch.float32)
+    def monitor_metrics(self, output, rating):
+        output = output.detach().cpu().numpy()
+        rating = rating.detach().cpu().numpy()
+        return {"rmse": np.sqrt(metrics.mean_squared_error(rating, output))}
 
-# Initialize the model
-embedding_dim = 128
-model = RecommendationModel(num_users, num_movies, embedding_dim)
+    def forward(self, users, movies, ratings=None):
+        user_embeds = self.user_embed(users)
+        movie_embeds = self.movie_embed(movies)
+        output = torch.cat([user_embeds, movie_embeds], dim=1)
+        output = self.out(output)
 
-# Define the loss function and optimizer
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+        loss = nn.MSELoss()(output, ratings.view(-1, 1))
+        metrics = self.monitor_metrics(output, ratings.view(-1, 1))
+        return output, loss, metrics
 
-# Training loop
-num_epochs = 10
-for epoch in range(num_epochs):
-    optimizer.zero_grad()
 
-    # Forward pass
-    outputs = model(user_indices, item_indices)
+def train():
+    df = pd.read_csv("ratings.csv")
 
-    # Compute the loss
-    loss = criterion(outputs, ratings)
+    lbl_user = preprocessing.LabelEncoder()
+    lbl_movie = preprocessing.LabelEncoder()
 
-    # Backward pass and optimization
-    loss.backward()
-    optimizer.step()
+    df.user = lbl_user.fit_transform(df.user.values)
+    df.movie = lbl_movie.fit_transform(df.movie.values)
 
-    # Print the loss for every epoch
-    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
+    df_train, df_valid = model_selection.train_test_split(df, test_size=0.1, random_state=42, stratify=df.rating.values)
+
+    train_dataset = MovieDataset(
+        users=df_train.user.values, movies=df_train.movie.values, ratings=df_train.rating.values
+    )
+
+    valid_dataset = MovieDataset(
+        users=df_valid.user.values, movies=df_valid.movie.values, ratings=df_valid.rating.values
+    )
+
+    model = RecSysModel(num_users=len(lbl_user.classes_), num_movies=len(lbl_movie.classes_))
+    model.fit(train_dataset, valid_dataset, train_bs=1024, valid_bs=1024, fp16=True)
+
+    torch.save(model.state_dict(), "recsys_model.pt")
+
+
+if __name__ == "__main__":
+    train()
